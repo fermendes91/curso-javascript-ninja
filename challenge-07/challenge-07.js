7477443adc239aab774f4fb76b6c496f1ebe7c3c
FernandoMendes <afmj.cartoes@outlook.com>
2018-12-26 14:28:49 -0200

    Challenge 07 - Fix
    Correção basica em uma parte do desafio.

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -16,7 +16,7 @@ function addItem(item) {
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-array = addItem([85, 'Mendes', true]);
+console.log(addItem([85, 'Mendes', true]));
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
