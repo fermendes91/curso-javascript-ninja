5d88f371a944f3f784d9a640ed11de4010754c75
FernandoMendes <afmj.cartoes@outlook.com>
2019-02-05 16:00:03 -0200

    Challenge 23 - Implementação

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -1,25 +1,77 @@
-/*
-Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
-As regras são:
+(function () {
+    'use strict';
 
-- Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
-diretamente;
-- O input deve iniciar com valor zero;
-- Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
-- Deve haver 4 botões para as operações principais: soma (+), subtração(-),
-multiplicação(x) e divisão(÷);
-- Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
-que irá limpar o input, deixando-o com valor 0;
+    /*
+    Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
+    As regras são:
 
-- A cada número pressionado, o input deve atualizar concatenando cada valor
-digitado, como em uma calculadora real;
-- Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
-operação no input. Se o último caractere no input já for um símbolo de alguma
-operação, esse caractere deve ser substituído pelo último pressionado.
-Exemplo:
-- Se o input tem os valores: "1+2+", e for pressionado o botão de
-multiplicação (x), então no input deve aparecer "1+2x".
-- Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
-input;
-- Ao pressionar o botão "CE", o input deve ficar zerado.
-*/
+    - Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
+    diretamente;
+    - O input deve iniciar com valor zero;
+    - Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
+    - Deve haver 4 botões para as operações principais: soma (+), subtração(-),
+    multiplicação(x) e divisão(÷);
+    - Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
+    que irá limpar o input, deixando-o com valor 0;
+
+    - A cada número pressionado, o input deve atualizar concatenando cada valor
+    digitado, como em uma calculadora real;
+    - Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
+    operação no input. Se o último caractere no input já for um símbolo de alguma
+    operação, esse caractere deve ser substituído pelo último pressionado.
+    Exemplo:
+    - Se o input tem os valores: "1+2+", e for pressionado o botão de
+    multiplicação (x), então no input deve aparecer "1+2x".
+    - Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
+    input;
+    - Ao pressionar o botão "CE", o input deve ficar zerado.
+    */
+
+    var $elements = document.querySelectorAll('button');
+    var $inputCalc = document.querySelector('#input-calc');
+    var $btnClearAll = document.querySelector('.all-clear');
+    var $btnEquals = document.querySelector('.equal-sign');
+    $elements.forEach(function(element) {
+        element.onclick = function(element) {
+            updateCalculatorInput(element.path[0].value);
+        }
+    });
+
+    function updateCalculatorInput(input) {
+        if(input === '=')
+            return;
+
+        var isOperator = verifyOperator(input);
+        var isLastValueOperator = verifyOperator($inputCalc.value.charAt($inputCalc.value.length-1));
+
+        if($inputCalc.value == 0){
+            if(!isOperator) {
+                $inputCalc.value = input;
+            } 
+        } else {
+            if(isOperator && isLastValueOperator) {
+                $inputCalc.value = $inputCalc.value.replace(/(.?)$/, input);
+            } else {
+                $inputCalc.value += input;
+            }
+        }
+            
+    }
+
+    function verifyOperator(char) {
+        return char == '+' || char == '-' || char == '*' || char == '/' || char == '.';
+    }
+
+    $btnClearAll.onclick = clearAll;
+    function clearAll() {
+        $inputCalc.value = 0;
+    }
+
+    $btnEquals.onclick = calculate;
+    function calculate() {
+        $inputCalc.value = eval($inputCalc.value);
+    }
+
+
+})();
+
