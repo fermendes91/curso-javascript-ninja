d97cb163b9ba6b5609f86eb477e4b5bef3794e57
FernandoMendes <afmj.cartoes@outlook.com>
2019-05-30 08:34:24 -0300

    Revert "Merge remote-tracking branch 'origin/challenge-32'"
    This reverts commit cf305b4ae3156fa9873f9a4fd18aa7bed917b938.

--- challenge-32/server/app.js
+++ challenge-32/server/app.js
@@ -7,34 +7,12 @@ var app = express();
 var port = process.env.PORT || 3000;
 var routes = require('./routes');
 
-var cars = [];
-
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.json({ message: 'hi' });
-});
-
-app.get('/cars', function(request, response) {
-  response.json(cars);
-});
-
-app.post('/car', function(request, response) {
-  var car = {
-    imagem: request.body.imagem,
-    marca: request.body.marca,
-    modelo: request.body.modelo,
-    ano: request.body.ano,
-    placa: request.body.placa,
-    cor: request.body.cor
-  }
-  console.log(car);
-
-  cars.push(car);
-
-  response.json(cars);
 });
 
 app.use('/car', routes);
